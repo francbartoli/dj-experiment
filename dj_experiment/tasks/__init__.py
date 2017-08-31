bd4a41ccc240a8e822a80fe8f1dbcc960ecd44e1
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-08-31 16:12:29 +0200

    Fix tasks example in reusable app

--- dj_experiment/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from dj_experiment.tasks.celery import app as celery_app  # noqa
-
-__all__ = ['celery_app']
-
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
--- dj_experiment/tasks/tasks.py
+++ dj_experiment/tasks/tasks.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import
 
 import time
 
-from dj_experiment.tasks.celery import app
+from celery import shared_task
 
 
-@app.task(name='tasks.longtime_add')
+@shared_task  # (name='longtime_add')
 def longtime_add(x, y):
     print 'long time task begins'
     # sleep 5 seconds
