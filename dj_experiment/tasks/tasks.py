ac972f86b1abfefec62d86374fcc8394cc3acaf1
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-09-01 11:59:10 +0200

    Add task to return an Experiment class

--- dj_experiment/tasks/tasks.py
+++ dj_experiment/tasks/tasks.py
@@ -3,12 +3,27 @@ from __future__ import absolute_import
 import time
 
 from celery import shared_task
+from celery.contrib import rdb
+from dj_experiment.models import Experiment as ModExperiment
+from dj_experiment.tasks.utils.extract_files import get_experiment
 
 
-@shared_task  # (name='longtime_add')
+@shared_task
 def longtime_add(x, y):
     print 'long time task begins'
     # sleep 5 seconds
     time.sleep(5)
     print 'long time task finished'
     return x + y
+
+
+@shared_task
+def netcdf_save(exp_id, dir):
+    print 'netcdf save task begins'
+    # do something
+    xpname = ModExperiment.objects.get(pk=exp_id)
+    xpinst = get_experiment(xpname, '')
+    rdb.set_trace()
+    print xpinst
+    print 'netcdf save task finished'
+    # return query count from db
---
+++ dj_experiment/tasks/utils/extract_files.py
@@ -0,0 +1,64 @@
+import ast
+import json
+import logging
+import sys
+from StringIO import StringIO
+
+from dj_experiment.management.commands.configurexperiment import \
+    Command as ConfigureExperiment
+from experiment import Case, Experiment, Field
+
+
+def get_experiment(namedexp, rootdirexp):
+
+    namedexp = 'RCM data'
+    rootdirexp = '\
+    /Users/geobart/Development/Fao/KnowledgeHub/dj-experiment/example/data'
+
+    # from IPython import embed; embed();
+    old_stdout = sys.stdout
+    # This variable will store everything that is sent to the
+    # standard output
+    cmd = StringIO()
+    sys.stdout = cmd
+
+    # Here we can call anything we like, like external modules,
+    # and everything that they will send to standard output will be
+    # stored on "cmd"
+    ce = ConfigureExperiment()
+    ce.handle(experiment=namedexp,
+              basepath=rootdirexp,
+              export=True)
+
+    # Redirect again the std output to screen
+    sys.stdout = old_stdout
+    xperkwargs = ast.literal_eval(cmd.getvalue())
+    logging.debug("xperkwargs======>%s" % xperkwargs)
+
+    # Try to instantiate cases
+    logging.debug("Reading cases")
+    try:
+        caseitems = xperkwargs['cases'].items()
+    except TypeError as e:
+        raise ValueError(
+            "Couldn't retrieve `caseitems` \
+            because `xperkwargs` ===> %s" % xperkwargs)
+        logging.debug("Exception is %s" % e)
+    cases = []
+    for case_short, case_kws in caseitems:
+        logging.debug("      {}: {}".format(case_short, case_kws))
+        cases.append(Case(case_short, **case_kws))
+    xperkwargs['cases'] = cases
+
+    # Try to instantiate fieldgroups
+    logging.debug("Reading fieldgroup")
+    fieldgroups = []
+    for fieldgroup_short, fieldgroup_kws in xperkwargs['fieldgroups'].items():
+        logging.debug("      {}: {}".format(fieldgroup_short, fieldgroup_kws))
+        fieldgroups.append(Field(fieldgroup_short, **fieldgroup_kws))
+    xperkwargs['fieldgroups'] = fieldgroups
+
+    # Create and return the Experiment
+    xperiment = Experiment(**xperkwargs)
+    logging.debug(xperiment.to_dict())
+    return xperiment
