19a8a0e2f85b7ab01cbd3e2dd283e8e1e9b97373
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-09-01 11:59:51 +0200

    Add the use of task to the example app

--- example/example/tasksapp/run_tasks.py
+++ example/example/tasksapp/run_tasks.py
@@ -1,6 +1,6 @@
 import time
 
-from dj_experiment.tasks.tasks import longtime_add
+from dj_experiment.tasks.tasks import longtime_add, netcdf_save
 
 if __name__ == '__main__':
     result = longtime_add.delay(1, 2)
@@ -12,3 +12,10 @@ if __name__ == '__main__':
     # now the task should be finished and ready method will return True
     print 'Task finished? ', result.ready()
     print 'Task result: ', result.result
+
+    result1 = netcdf_save.delay(14, '')
+    print 'Task netcdf finished? ', result1.ready()
+    print 'Task result1: ', result1.result
+    time.sleep(10)
+    print 'Task netcdf finished? ', result1.ready()
+    print 'Task result1: ', result1.result
