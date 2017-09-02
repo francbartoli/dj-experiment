2fc45a6a0e2ba1efe06b4282234cf13c0ccd5b7d
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-09-02 16:12:25 +0200

    Add default base data dir for experiments

--- dj_experiment/conf.py
+++ dj_experiment/conf.py
@@ -1,9 +1,12 @@
+import os
+
 from appconf import AppConf
 from django.conf import settings
 
 
 class DjExperimentAppConf(AppConf):
     DATA_DIR = "./"
+    BASE_DATA_DIR = os.path.join(settings.BASE_DIR, 'data')
     SEPARATOR = "."
     OUTPUT_PREFIX = ""
     OUTPUT_SUFFIX = ".nc"
